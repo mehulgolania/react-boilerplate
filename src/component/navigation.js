f415c0a8a2cba1cc3fc2b330d985c68e2c47682a
mehul_golania <mehul.golania@vshift.com>
2019-06-28 18:42:17 +0530

    Active class issue resolved.
    Two active class was appearing in the navigation.

--- src/component/navigation.js
+++ src/component/navigation.js
@@ -14,9 +14,9 @@ function Navigation() {
 
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
           <Nav>
-            <LinkContainer to="/"><Nav.Link>Home</Nav.Link></LinkContainer>
-            <LinkContainer to="/about"><Nav.Link>About</Nav.Link></LinkContainer>
-            <LinkContainer to="/contact"><Nav.Link>Contact</Nav.Link></LinkContainer>
+            <LinkContainer to="/"><Nav.Link active={false}>Home</Nav.Link></LinkContainer>
+            <LinkContainer to="/about"><Nav.Link active={false}>About</Nav.Link></LinkContainer>
+            <LinkContainer to="/contact"><Nav.Link active={false}>Contact</Nav.Link></LinkContainer>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
